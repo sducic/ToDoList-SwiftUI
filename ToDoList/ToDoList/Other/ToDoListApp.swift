ab2d5f32a0648ee9e5d81962ce62d9602e0a55a2
sducic <59136829+sducic@users.noreply.github.com>
2025-03-24 18:54:24 +0100

    Firebase config

--- ToDoList/ToDoList/Other/ToDoListApp.swift
+++ ToDoList/ToDoList/Other/ToDoListApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
