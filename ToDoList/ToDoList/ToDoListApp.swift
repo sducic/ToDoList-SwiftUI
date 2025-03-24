885977ddefbf39980c80be33e07d52c56e940d8e
sducic <59136829+sducic@users.noreply.github.com>
2025-03-24 16:19:26 +0100

    Create project

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Stefan Ducic on 24.3.25..
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
