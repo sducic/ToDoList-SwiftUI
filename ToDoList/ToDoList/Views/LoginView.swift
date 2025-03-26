0c75ec95130cef6e6599d0c87be671fb88df1a26
Stefan Ducic <ducic_stefan@hotmail.com>
2025-03-26 16:50:45 +0100

    Add comments to LoginView

--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         VStack {
-            
+            //header
             HeaderView()
+            
+            //login form
+            Form {
+                //TextField("Email Adress", text: @email)
+                //SecureField("")
+            }
             
         }
     }
