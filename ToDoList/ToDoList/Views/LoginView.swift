c9cb490857d71d6dfa72b5e03e961a64837bf65f
Stefan Ducic <ducic_stefan@hotmail.com>
2025-03-27 15:40:52 +0100

    Implement LoginView

--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var pass = ""
+    
     var body: some View {
         VStack {
             //header
@@ -15,10 +19,34 @@ struct LoginView: View {
             
             //login form
             Form {
-                //TextField("Email Adress", text: @email)
-                //SecureField("")
+                TextField("Email Adress" , text: $email )
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $pass)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                Button  {
+                    
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
             }
             
+            VStack {
+                Text("New around here")
+                Button("Create An Account") {
+                    //show registration
+                }
+            }
+            .padding(.bottom,50)
+            
+            
+            Spacer()
         }
     }
 }
